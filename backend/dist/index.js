6416657637950587585cd41efd9962d3d9d7ea6d
Subhamay-Dey <ncdey1966@gmail.com>
2025-03-22 04:06:12 +0530

    Set up Express server with EJS view engine and added welcome page and added new npm commands✅

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,8 +1,17 @@
 import express from "express";
+import cors from "cors";
 import "dotenv/config";
+import path from "path";
+import { fileURLToPath } from "url";
 const app = express();
 const PORT = process.env.PORT || 7000;
+app.use(cors({ origin: "*", credentials: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "./views"));
 app.get("/", (req, res) => {
-    res.send("Server is running...🙌");
+    res.render("welcome");
 });
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,9 @@
 import express, {Application, Request, Response} from "express";
 import cors from "cors";
 import "dotenv/config";
+import path from "path";
+import {fileURLToPath} from "url"
+
 
 const app:Application = express();
 
@@ -10,8 +13,12 @@ app.use(cors({origin: "*", credentials: true}));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "./views"));
+
 app.get("/", (req: Request, res: Response) => {
-    res.send("Server is running...🙌")
+    res.render("welcome");
 })
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
