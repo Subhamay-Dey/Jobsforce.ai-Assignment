99641c9825191fcee7baf4d7701c0f4abda7b33d
Subhamay-Dey <ncdey1966@gmail.com>
2025-03-22 21:14:13 +0530

    Add BullMQ Redis Connection and DefaultQueueOptions integration for job queue management ✅

---
+++ backend/dist/bull/queue.js
@@ -0,0 +1,17 @@
+export const redisConnection = {
+    host: process.env.REDIS_HOST,
+    port: Number(process.env.REDIS_PORT) || 6379,
+    password: process.env.UPSTASH_REDIS_PASSWORD
+};
+export const defaultQueueOptions = {
+    removeOnComplete: {
+        count: 1000,
+        age: 60 * 60,
+    },
+    attempts: 5,
+    backoff: {
+        type: "exponential",
+        delay: 5000,
+    },
+    removeOnFail: false,
+};
