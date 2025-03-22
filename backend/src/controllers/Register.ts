af8fb8532844c1f1346290d71203d15a0f693679
Subhamay-Dey <ncdey1966@gmail.com>
2025-03-22 22:42:27 +0530

    Add Zod validation schema for user registration and implement Register controller ✅

---
+++ backend/src/controllers/Register.ts
@@ -0,0 +1,9 @@
+import {Request, Response} from "express";
+
+class Register {
+    static async register(req: Request, res: Response){
+        const body = req.body;
+    }
+}
+
+export default Register;
