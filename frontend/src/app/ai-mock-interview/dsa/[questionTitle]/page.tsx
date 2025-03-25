3c8c04f870a8bf98d0888e86d1093879336e42c5
Subhamay-Dey <ncdey1966@gmail.com>
2025-03-25 10:13:45 +0530

    Add Avatar and Theme/Language selector components; update Header layout and styles ✅

--- frontend/src/app/ai-mock-interview/dsa/[questionTitle]/page.tsx
+++ frontend/src/app/ai-mock-interview/dsa/[questionTitle]/page.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 function page() {
   return (
     <div className='h-screen'>
-      <div className='max-w-[1800px] max-auto py-4 px-5'>
+      <div className='w-full max-auto py-4 px-5'>
         <Header/>
       </div>
       <DsaQuestion/>
